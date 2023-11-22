6c63117021d2736a34a902ef7e1249efe85e812f
omarcosss <89608993+omarcosss@users.noreply.github.com>
2023-11-22 14:33:54 -0300

    inicio

---
+++ main.js
@@ -0,0 +1,52 @@
+searchForm = document.querySelector('.form-busca');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+
+window.onscroll = () =>{
+    if(window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
